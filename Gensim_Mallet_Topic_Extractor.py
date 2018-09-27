7e22e195de6eb247f60b5d1582b5e7fca891400a
Drakael <drakael@hotmail.com>
2018-09-27 14:50:54 +0200

    small corrections, prettier tracing, gem mint PEP8

--- Gensim_Mallet_Topic_Extractor.py
+++ Gensim_Mallet_Topic_Extractor.py
@@ -33,12 +33,10 @@ gmte.get_topic_distribution()
 """
 
 import os
-import numpy as np
 import pandas as pd
 from pprint import pprint
 
 # Gensim
-import gensim
 import gensim.corpora as corpora
 from gensim.utils import simple_preprocess
 from gensim.models.phrases import Phrases, Phraser
@@ -60,15 +58,14 @@ import matplotlib.pyplot as plt
 
 # Enable logging for gensim - optional
 import logging
+import warnings
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.ERROR)
-
-import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-class Gensim_Mallet_Topic_Extractor():
-    def __init__(self, language='english'):
+class GensimMalletTopicExtractor:
+    def __init__(self, language='english', stopwords_extent=None):
         self.language2la = {
             'english': 'en',
             'french': 'fr',
@@ -78,10 +75,36 @@ class Gensim_Mallet_Topic_Extractor():
             raise ValueError('Language must be "english", "french" or "spanish"')
         self.language = language
         self.stop_words = stopwords.words(self.language)
-        self.stop_words.extend(['from', 'subject', 're', 'edu', 'use',
-                                'nntp-posting-host'])
+        if stopwords_extent is str or stopwords_extent is list:
+            self.stop_words.extend(stopwords_extent)
+        self.df_topic_sents_keywords = None
+        self.bigram = None
+        self.bigram_mod = None
+        self.trigram = None
+        self.trigram_mod = None
+        self.vis = None
+        self.data = None
+        self.data_words = None
+        self.data_words_nostops = None
+        self.data_words_bigrams = None
+        self.data_words_trigrams = None
+        self.nlp = None
+        self.data_lemmatized = None
+        self.id2word = None
+        self.texts = None
+        self.corpus = None
+        self.mallet_path = None
+        self.lda_model = None
+        self.coherence_model_lda = None
+        self.coherence_lda = None
+        self.coherence_values = []
+        self.model_list = []
+        self.optimal_number_of_topics = None
+        self.optimal_model = None
+        self.optimal_topics = None
 
-    def sent_to_words(self, sentences, remove_punctuation=True):
+    @staticmethod
+    def sent_to_words(sentences, remove_punctuation=True):
         for sentence in sentences:
             # deacc=True removes punctuations
             yield(simple_preprocess(str(sentence), deacc=remove_punctuation))
@@ -99,7 +122,9 @@ class Gensim_Mallet_Topic_Extractor():
         self.trigram_mod = Phraser(self.trigram)
         return [self.trigram_mod[self.bigram_mod[doc]] for doc in texts]
 
-    def lemmatization(self, texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    def lemmatization(self, texts, allowed_postags=None):
+        if allowed_postags is None:
+            allowed_postags = ['NOUN', 'ADJ', 'VERB', 'ADV']
         """https://spacy.io/api/annotation"""
         texts_out = []
         for sent in texts:
@@ -109,7 +134,7 @@ class Gensim_Mallet_Topic_Extractor():
 
     def view_terms_frequency(self, text_id, first_words=20):
         # Human readable format of corpus (term-frequency)
-        list_ = [[(self.id2word[id], freq) for id, freq in text[:first_words]] for text in self.corpus[text_id]]
+        list_ = [[(self.id2word[id_], freq) for id_, freq in text[:first_words]] for text in self.corpus[text_id]]
         pprint(list_)
 
     def visualize_lda(self):
@@ -118,29 +143,39 @@ class Gensim_Mallet_Topic_Extractor():
         self.vis = pyLDAvis.gensim.prepare(self.lda_model, self.corpus, self.id2word)
         print(self.vis)
 
-    def extract_topics(self, data, num_topics, enable_mallet=True):
+    def extract_topics(self, data, num_topics, passes=600,
+                       enable_mallet=True, optimize_interval=0):
+        self.data = data
         print('\nEXTRACTING ' + str(num_topics) + ' TOPICS')
-        self.data_words = list(self.sent_to_words(data, True))
+        self.data_words = list(self.sent_to_words(self.data, True))
         # Remove Stop Words
+        print('\nRemoving stopwords')
         self.data_words_nostops = self.remove_stopwords(self.data_words)
         # Form Bigrams
+        print('Looking for bigrams')
         self.data_words_bigrams = self.make_bigrams(self.data_words_nostops)
-        # Form Bigrams
+        # Form Trigrams
+        print('Looking for trigrams')
         self.data_words_trigrams = self.make_trigrams(self.data_words_bigrams)
         # Initialize spacy 'en' model, keeping only tagger component (for efficiency)
         # python3 -m spacy download en
+        print('Loading Spacy with ' + self.language + ' dictionary')
         self.nlp = spacy.load(self.language2la[self.language], disable=['parser', 'ner'])
         # Do lemmatization keeping only noun, adj, vb, adv
+        print('Lemmatizing')
         self.data_lemmatized = self.lemmatization(self.data_words_trigrams,
                                                   allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
         # Create Dictionary
+        print('Creating dictionary')
         self.id2word = corpora.Dictionary(self.data_lemmatized)
         # Create Corpus
+        print('Creating corpus')
         self.texts = self.data_lemmatized
         # Term Document Frequency
+        print('Computing document frequency')
         self.corpus = [self.id2word.doc2bow(text) for text in self.texts]
         # Build LDA model
-        print('enable_mallet', enable_mallet)
+        print('\nEnable_mallet is', enable_mallet, '\n')
         if enable_mallet is True:
             # Download File: http://mallet.cs.umass.edu/dist/mallet-2.0.8.zip
             os.environ.update({'MALLET_HOME': r'C:/mallet-2.0.8/'})
@@ -148,8 +183,10 @@ class Gensim_Mallet_Topic_Extractor():
             self.lda_model = LdaMallet(self.mallet_path,
                                        corpus=self.corpus,
                                        num_topics=num_topics,
-                                       id2word=self.id2word)
-            print('built Mallet LDA model')
+                                       id2word=self.id2word,
+                                       iterations=passes,
+                                       optimize_interval=optimize_interval)
+            print('Mallet LDA model built\n')
             pprint(self.lda_model.show_topics(formatted=False))
         else:
             self.lda_model = LdaMulticore(corpus=self.corpus,
@@ -158,10 +195,10 @@ class Gensim_Mallet_Topic_Extractor():
                                           random_state=100,
                                           # update_every=1,
                                           chunksize=100,
-                                          passes=10,
+                                          passes=passes,
                                           alpha='auto',
                                           per_word_topics=True)
-            print('built LDA model')
+            print('LDA model built\n')
             pprint(self.lda_model.print_topics())
         # print(self.lda_model[self.corpus])
         # Compute Perplexity
@@ -170,7 +207,7 @@ class Gensim_Mallet_Topic_Extractor():
             print('\nPerplexity: ', self.lda_model.log_perplexity(self.corpus))
 
         # Compute Coherence Score
-        print('Computing coherence model')
+        print('\nComputing coherence model')
         self.coherence_model_lda = CoherenceModel(model=self.lda_model,
                                                   texts=self.data_lemmatized,
                                                   dictionary=self.id2word,
@@ -183,12 +220,10 @@ class Gensim_Mallet_Topic_Extractor():
             self.visualize_lda()
 
     def view_optimal_topics(self, num_words=20):
-        # Select the model and print the topics
-        self.optimal_model = self.model_list[np.argmax(self.coherence_values)]
-        self.optimal_topics = self.optimal_model.show_topics(formatted=False)
         pprint(self.optimal_model.print_topics(num_words=num_words))
 
-    def compute_coherence_values(self, limit, start=2, step=3, enable_mallet=True):
+    def compute_coherence_values(self, limit, start=2, step=3, passes=600,
+                                 enable_mallet=True, optimize_interval=0):
         """
         Compute c_v coherence for various number of topics
 
@@ -201,15 +236,15 @@ class Gensim_Mallet_Topic_Extractor():
         model_list : List of LDA topic models
         coherence_values : Coherence values corresponding to the LDA model with respective number of topics
         """
-        self.coherence_values = []
-        self.model_list = []
         for num_topics in range(start, limit, step):
             print('\n' + '*'*10 + ' COMPUTING COHERENCE FOR ' + str(num_topics) + ' TOPICS ' + '*'*10)
             if enable_mallet is True:
                 model = LdaMallet(self.mallet_path,
                                   corpus=self.corpus,
                                   num_topics=num_topics,
-                                  id2word=self.id2word)
+                                  id2word=self.id2word,
+                                  iterations=passes,
+                                  optimize_interval=optimize_interval)
             else:
                 model = LdaMulticore(corpus=self.corpus,
                                      id2word=self.id2word,
@@ -217,14 +252,14 @@ class Gensim_Mallet_Topic_Extractor():
                                      random_state=100,
                                      # update_every=1,
                                      chunksize=100,
-                                     passes=10,
+                                     passes=passes,
                                      alpha='auto',
                                      per_word_topics=True)
             self.model_list.append(model)
             coherence_model = CoherenceModel(model=model,
-                                            texts=self.data_lemmatized,
-                                            dictionary=self.id2word,
-                                            coherence='c_v')
+                                             texts=self.data_lemmatized,
+                                             dictionary=self.id2word,
+                                             coherence='c_v')
             self.coherence_values.append(coherence_model.get_coherence())
 
         # Show graph
@@ -232,12 +267,21 @@ class Gensim_Mallet_Topic_Extractor():
         plt.plot(x, self.coherence_values)
         plt.xlabel("Num Topics")
         plt.ylabel("Coherence score")
-        plt.legend(("coherence_values"), loc='best')
+        plt.legend("coherence_values", loc='best')
         plt.show()
 
         # Print the coherence scores
         for m, cv in zip(x, self.coherence_values):
             print("Num Topics =", m, " has Coherence Value of", round(cv, 4))
+
+        optimal_model_index = self.coherence_values.index(max(self.coherence_values))
+        self.optimal_number_of_topics = start + optimal_model_index
+        self.optimal_model = self.model_list[optimal_model_index]
+        print('\nOptimal number of topics is ' + str(self.optimal_number_of_topics) +
+              ' with coherence score : ' + str(self.coherence_values[optimal_model_index]))
+        self.optimal_topics = self.optimal_model.show_topics(num_topics=self.optimal_number_of_topics,
+                                                             num_words=20, formatted=False)
+        self.view_optimal_topics()
 
     def format_topics_sentences(self, ldamodel=None):
         if ldamodel is None and self.optimal_model is not None:
@@ -266,13 +310,14 @@ class Gensim_Mallet_Topic_Extractor():
         # Add original text to the end of the output
         contents = pd.Series(self.data)
         sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-        return(sent_topics_df)
+        return sent_topics_df
 
     def get_most_representative_documents(self):
         # Group top 5 sentences under each topic
         sent_topics_sorteddf_mallet = pd.DataFrame()
 
-        self.df_topic_sents_keywords = self.format_topics_sentences()
+        if self.df_topic_sents_keywords is None:
+            self.df_topic_sents_keywords = self.format_topics_sentences()
         # Format
         df_dominant_topic = self.df_topic_sents_keywords.reset_index()
         df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
@@ -294,6 +339,8 @@ class Gensim_Mallet_Topic_Extractor():
             print(i, sent_topics_sorteddf_mallet.loc[i, 'Text'])
 
     def get_topic_distribution(self):
+        if self.df_topic_sents_keywords is None:
+            self.df_topic_sents_keywords = self.format_topics_sentences()
         # Number of Documents for Each Topic
         topic_counts = self.df_topic_sents_keywords['Dominant_Topic'].value_counts()
         # Percentage of Documents for Each Topic
--- Newsgroups_Gensim_Extraction.py
+++ Newsgroups_Gensim_Extraction.py
@@ -1,23 +1,25 @@
 import re
 import pandas as pd
 from pprint import pprint
-from Gensim_Mallet_Topic_Extractor import Gensim_Mallet_Topic_Extractor
+from Gensim_Mallet_Topic_Extractor import GensimMalletTopicExtractor
 
 if __name__ == '__main__':
     # freeze_support()
 
-    gmte = Gensim_Mallet_Topic_Extractor('english')
+    gmte = GensimMalletTopicExtractor('english')
 
     # Import Dataset
     df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
-    print(df.target_names.unique())
-    df.head()
+    print(df.target_names.unique(), '\n')
 
     # Convert to list
     data = df.content.values.tolist()
 
     # Remove Emails
     data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+
+    # Remove Nntp-Posting-Host
+    data = [sent.replace('Nntp-Posting-Host', '') for sent in data]
 
     # Remove new line characters
     data = [re.sub('\s+', ' ', sent) for sent in data]
@@ -27,17 +29,19 @@ if __name__ == '__main__':
 
     pprint(data[:1])
 
-    gmte.extract_topics(data, 20)
+    gmte.extract_topics(data, 21, passes=600, optimize_interval=10)
 
     # Can take a long time to run.
-    gmte.compute_coherence_values(start=2, limit=40, step=1)
-
+    gmte.compute_coherence_values(start=19, limit=33, step=1,
+                                  passes=600,
+                                  optimize_interval=10)
 
     df_topic_sents_keywords = gmte.format_topics_sentences()
 
     # Format
     df_dominant_topic = df_topic_sents_keywords.reset_index()
-    df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+    df_dominant_topic.columns = ['Document_No', 'Dominant_Topic',
+                                 'Topic_Perc_Contrib', 'Keywords', 'Text']
 
     # Show
     df_dominant_topic.head(10)
